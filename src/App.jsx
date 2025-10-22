c2faf6762e6d129b69bfdab2252b13c16b52f86f
DilshanMs <ar112318@fhss.sjp.ac.lk>
2025-10-23 02:15:59 +0530

    feat: Initialize Projects component with basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <main>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
           </Routes>
         </main>
       </div>
